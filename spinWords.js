7108bc96e9710c2f4af68e9a077f05ab2b17e378
aboudaqrabawi <aqrabawijoe@gmail.com>
2021-03-26 21:02:42 +0300

    spinWords

--- spinWords.js
+++ spinWords.js
@@ -8,20 +8,3 @@
 
 // spinWords( "This is another test" )=> returns "This is rehtona test"
 
-
-function spinWords(str){
-    var newstr= str.split(' ')
-    var answer=[]
-    
-    for (var i = 0 ; i < newstr.length;i++){
-      if (newstr[i].length>=5){
-        answer.push(newstr[i].split('').reverse().join(''))
-      } else{
-         answer.push(newstr[i])
-      }
-     
-    }
-   
-    return  answer.join(' ')
-   
-  }
